623daa7de7026347525bbd8067445cd025af9b07
Hotrung2311 <trungho2311@gmail.com>
2020-04-24 11:54:42 +0700

    Viết lại binary saerch theo ý hiểu của mình

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,21 +1,35 @@
 public class BinarySearch {
     static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
 
-    static int BinarySearch(int[] list, int key) {
-        int low = 0;
-        int high = list.length - 1;
-        while (high >= low) {
-            int mid = (low + high) / 2;
-            if (key < list[mid]){
-                high = mid - 1;
-            }else if (key == list[mid]){
+    //    static int BinarySearch(int[] list, int key) {
+//        int low = 0;
+//        int high = list.length - 1;
+//        while (high >= low) {
+//            int mid = (low + high) / 2;
+//            if (key < list[mid]){
+//                high = mid - 1;
+//            }else if (key == list[mid]){
+//                return mid;
+//            }else {
+//                low = mid + 1;
+//            }
+//        }
+//        return -1;
+//    }
+    static int BinarySearch(int[] list, int expectValue) {
+        int first = 0;
+        int last = list.length - 1;
+        while (first <= last) {
+            int mid = (first + last) / 2;
+            if (expectValue == list[mid]) {
                 return mid;
-            }else {
-                low = mid + 1;
-            }
+            } else if (expectValue > list[mid]) {
+                first = mid + 1;
+            } else last = mid - 1;
         }
         return -1;
     }
+
     public static void main(String[] args) {
         System.out.println(BinarySearch(list, 2));  /* 0 */
         System.out.println(BinarySearch(list, 4)); /* 4 */
@@ -31,5 +45,5 @@ public class BinarySearch {
         System.out.println(BinarySearch(list, 70)); /*-1 */
         System.out.println(BinarySearch(list, 79)); /*-1 */
     }
-    
+
 }
